6a6c16d8358d8c6aa4a91d609fa493b9d38b29ba
Coder-IsiahJones <jones.isiah.blhs@gmail.com>
2021-02-10 17:42:29 -0500

    Add project files.

---
+++ Services/SecurityService.cs
@@ -0,0 +1,25 @@
+﻿using RegisterandLoginExp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterandLoginExp.Services
+{
+    public class SecurityService
+    {
+        UsersDAO userDAO = new UsersDAO();
+
+        public SecurityService()
+        {
+           
+
+        }
+
+        public bool IsValid(UserModel user)
+        {
+            // return value if found in list
+            return userDAO.FindUserByNameAndPassword(user);
+        }
+    }
+}
